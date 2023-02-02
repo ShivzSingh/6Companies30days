6e4ce39145b03edd374e139a96256c5bd630667c
ShivzSingh <89547756+ShivzSingh@users.noreply.github.com>
2023-02-02 17:26:15 +0530

    Time: 555 ms (18.63%), Space: 18.3 MB (6.76%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int idx , vector<int>& nums , vector<int> &dp){
+        if(idx >= nums.size()-1){
+            return 0;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        int min_jumps =10001;
+        for(int j = 1 ; j <= nums[idx] ; j++){
+            if(idx + j <= nums.size() - 1){
+                min_jumps = min(min_jumps , 1 + f(idx + j , nums , dp));
+            }
+        }
+        return dp[idx] = min_jumps;
+    }
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1 , -1);
+        return f(0 , nums , dp);
+    }
+};
