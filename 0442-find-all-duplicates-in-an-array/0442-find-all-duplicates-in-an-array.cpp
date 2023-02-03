82e8ee8df6dd2cd9a342a5dcd03492378ca035cb
ShivzSingh <89547756+ShivzSingh@users.noreply.github.com>
2023-02-03 11:51:27 +0530

    Time: 65 ms (76.75%), Space: 33.5 MB (82.15%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -4,10 +4,14 @@ public:
         int n = nums.size();
         //unordered_map<int,int> mp;
         vector<int> ans;
-        sort(nums.begin() , nums.end());
-        for(int i = 0 ; i < n-1 ; i++){
-            if(nums[i] == nums[i+1]){
-                ans.push_back(nums[i]);
+        //sort(nums.begin() , nums.end());
+        for(int i = 0 ; i < n ; i++){
+            int idx = (abs(nums[i]) - 1);
+            if(nums[idx] < 0){
+                ans.push_back(idx+1);
+            }
+            else{
+                nums[idx] = -1*nums[idx];
             }
         }
         return ans;
