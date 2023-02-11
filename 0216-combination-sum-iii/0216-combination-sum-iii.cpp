2781041e27a83ca01d94a2d9cb33a8de94965fea
ShivzSingh <89547756+ShivzSingh@users.noreply.github.com>
2023-02-11 13:33:59 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (64.89%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void CombinationSum(int ind, vector<int> &ds, vector<vector<int>> &ans, int target, int k, int n){
-        if(ds.size()==k){
-            if(target==0){
-                ans.emplace_back(ds);
-                return;
+    vector<vector<int>> ans;
+    void f(int idx , vector<int> &nums , vector<int> &ds , int target , int k){
+        if(idx >= nums.size()){
+            if(k == 0 && target == 0){
+                ans.push_back(ds);
             }
+            return;
         }
-        for(int i=ind;i<=9;i++){
-            if(ds.size()>=k && target!=0) break;
-            if(i>target) break;
-            ds.emplace_back(i);
-            CombinationSum(i+1, ds, ans, target-i, k, n);
+        if(nums[idx] <= target){
+            ds.push_back(nums[idx]);
+            f(idx+1 , nums , ds , target - nums[idx] , k-1);
             ds.pop_back();
         }
+        f(idx+1 , nums, ds , target , k);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
+        vector<int> nums;
         vector<int> ds;
-        if(n<k){
-            return ans;
+        for(int i = 1 ; i <= 9 ; i++){
+            nums.push_back(i);
         }
-        CombinationSum(1, ds, ans, n, k, n);
+        f(0 , nums , ds , n , k);
         return ans;
     }
 };
