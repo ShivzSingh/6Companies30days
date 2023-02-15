147b2754f00daf6322811d8733467daffb266ae5
ShivzSingh <89547756+ShivzSingh@users.noreply.github.com>
2023-02-15 13:06:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,20 +5,12 @@ public:
             return 1;
         }
         if(dp[idx] != -1){
-            return  dp[idx];
+            return dp[idx];
         }
-        
         return dp[idx] = f(idx-1 , dp) + f(idx-2 , dp);
     }
     int climbStairs(int n) {
-        //vector<int> dp(n+1 , 0);
-        int prev2 = 1;
-        int prev = 1;
-        for(int i = 2 ; i <= n ; i++){
-            int curr = prev2 + prev;
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        vector<int> dp(n+1 , -1);
+        return f(n , dp);
     }
 };
